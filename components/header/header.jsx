31dbe7cb549e6c85e01337eb8bf7bf9311184119
davistocco <davistocco@outlook.com>
2022-07-12 19:01:44 -0300

    Header contact and social medias

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -1,3 +1,5 @@
+import { faFacebook, faInstagram, faWhatsapp, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Image from 'next/image';
 import React from 'react'
 import styles from './header.module.scss';
@@ -11,10 +13,24 @@ export default function Header() {
                     <li><a href="#inicio">Início</a></li>
                     <li><a href="#pedido-de-oracao">Pedido de oração</a></li>
                     <li><a href="#nossa-localizacao">Nossa localização</a></li>
-                    <li>Encontre um gc</li>
+                    <li><a href="">Encontre um GC</a></li>
+                    <li><a href="">Nosso APP</a></li>
                 </ul>
                 <div className={styles.extra}>
-                    <button className={`${styles.btn} ${styles['btn-transparent']}`}>Fale com a gente</button>
+                    <div className={styles.contact}>
+                        <a href="https://wa.me/5516994057448/" target='_blank'>
+                            <FontAwesomeIcon icon={faWhatsapp} />
+                        </a>
+                        <a href="https://www.instagram.com/lagoinharibeiraopreto/" target='_blank'>
+                            <FontAwesomeIcon icon={faInstagram} />
+                        </a>
+                        <a href="https://www.facebook.com/LagoinhaRibeiraoPreto/" target='_blank'>
+                            <FontAwesomeIcon icon={faFacebook} />
+                        </a>
+                        <a href="https://www.youtube.com/c/IgrejaBatistaLagoinhaRibeir%C3%A3oPretoSP" target='_blank'>
+                            <FontAwesomeIcon icon={faYoutube} />
+                        </a>
+                    </div>
                     <button className={`${styles.btn} ${styles['btn-primary']}`}>Contribua</button>
                 </div>
             </header>
