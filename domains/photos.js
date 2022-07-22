a81add0350afbf1ac6ba861126dda5a31b591bc3
Davi Stocco <davistocco@outlook.com>
2022-07-22 19:16:42 -0300

    null check photos strapi

--- domains/photos.js
+++ domains/photos.js
@@ -3,9 +3,9 @@ import strapi from "../services/strapi";
 const getPhotos = () => new Promise((resolve, reject) => {
     strapi.get('/photo?populate=img')
         .then(response => {
-            const photos = response.data.data.attributes.img.data.map(image => ({
+            const photos = response?.data?.data?.attributes?.img?.data?.map(image => ({
                 url: process.env.NEXT_PUBLIC_STRAPI_URL + image.attributes.formats.medium.url
-            }))
+            })) ?? [];
 
             resolve(photos);
         })
