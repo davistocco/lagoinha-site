41e8b9dce10ffd219492a0fa64655300f9420f17
Davi Stocco <davistocco@outlook.com>
2022-07-22 14:10:01 -0300

    responsive mobile menu

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -1,27 +1,69 @@
 import { faFacebook, faInstagram, faWhatsapp, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Image from 'next/image';
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './header.module.scss';
 import Link from 'next/link'
+import { faBars, faHamburger } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
+    const [show, setShow] = useState(false);
+
     useEffect(() => {
         let prevScrollpos = window.pageYOffset;
-        window.onscroll = function() {
-        let currentScrollPos = window.pageYOffset;
-          if (prevScrollpos > currentScrollPos) {
-            document.getElementById("header").style.top = "0";
-          } else {
-            document.getElementById("header").style.top = "-110px";
-          }
-          prevScrollpos = currentScrollPos;
+        window.onscroll = function () {
+            let currentScrollPos = window.pageYOffset;
+            if (prevScrollpos > currentScrollPos) {
+                document.getElementById("header").style.top = "0";
+            } else {
+                document.getElementById("header").style.top = "-110px";
+            }
+            prevScrollpos = currentScrollPos;
         }
     }, []);
-    
+
+    const openMobileMenu = () => {
+        setShow(!show);
+    }
+
     return (
-        <div className={styles.wrapper} id='header'>
-            <header className={styles.header}>
+        <>
+            <div className={styles.wrapper} id='header'>
+                <header className={styles.header}>
+                    <Link href='/'>
+                        <Image className={styles.logo} src="/images/Graf_Horiz_Black.png" alt="logo" width='200px' height='100px' objectFit='contain' />
+                    </Link>
+                    <ul className={styles.links}>
+                        <li><Link href="/#">Início</Link></li>
+                        <li><Link href="/#pedido-de-oracao">Pedido de oração</Link></li>
+                        <li><Link href="/#nossa-localizacao">Nossa localização</Link></li>
+                        <li><Link href="/#nosso-app">Nosso APP</Link></li>
+                        <li><Link href='/encontre-um-gc'>Encontre um GC</Link></li>
+                    </ul>
+                    <div className={styles.extra}>
+                        <div className={styles.contact}>
+                            <a href="https://wa.me/5516994057448/" target='_blank' rel="noreferrer">
+                                <FontAwesomeIcon icon={faWhatsapp} />
+                            </a>
+                            <a href="https://www.instagram.com/lagoinharibeiraopreto/" target='_blank' rel="noreferrer">
+                                <FontAwesomeIcon icon={faInstagram} />
+                            </a>
+                            <a href="https://www.facebook.com/LagoinhaRibeiraoPreto/" target='_blank' rel="noreferrer">
+                                <FontAwesomeIcon icon={faFacebook} />
+                            </a>
+                            <a href="https://www.youtube.com/c/IgrejaBatistaLagoinhaRibeir%C3%A3oPretoSP" target='_blank' rel="noreferrer">
+                                <FontAwesomeIcon icon={faYoutube} />
+                            </a>
+                        </div>
+                        <button className={`${styles.btn} ${styles['btn-primary']}`}> <Link href="/#contribua">Contribua</Link> </button>
+                    </div>
+                </header>
+            </div>
+
+            <header className={`${styles.mobile}  ${show && styles.show}`}>
+                <div className={styles.hamburger} onClick={openMobileMenu}>
+                    <FontAwesomeIcon icon={faBars} size='2x' />
+                </div>
                 <Link href='/'>
                     <Image className={styles.logo} src="/images/Graf_Horiz_Black.png" alt="logo" width='200px' height='100px' objectFit='contain' />
                 </Link>
@@ -50,6 +92,6 @@ export default function Header() {
                     <button className={`${styles.btn} ${styles['btn-primary']}`}> <Link href="/#contribua">Contribua</Link> </button>
                 </div>
             </header>
-        </div>
+        </>
     )
 }
