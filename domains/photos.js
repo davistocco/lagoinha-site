d878a1d4b2edac93bac4abf43e93f71bb2797f49
Davi Stocco <davistocco@outlook.com>
2023-11-29 10:46:51 -0300

    static page generation timeout

--- domains/photos.js
+++ domains/photos.js
@@ -4,7 +4,7 @@ const getPhotos = () => new Promise((resolve, reject) => {
         'https://lh3.googleusercontent.com/u/0/drive-viewer/AK7aPaDQeQQUvK5-RE0rJ5YeCoHrHDF8PiYqK4gVE3Q29d2ulrCgSenYVmumhECPlijQHKmF8lIUg8qUQfCXVcUAsYl_RrmSKQ=w1366-h619',
         'https://lh3.googleusercontent.com/u/0/drive-viewer/AK7aPaAb5A4tYKZAlUUEpJ9UMGWe-m3Eux2gPhXRBPK6DAvaVgHy6KolTOalTRfOfHiQEv2AwKRvJoNP8x1JQcsEDOqiHm5TCg=w1366-h619',
     ];
-    return urls.map(url => ({ url }));
+    resolve(urls.map(url => ({ url })));
 });
 
 export {
--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
-
+  staticPageGenerationTimeout: 1000
 }
 
 module.exports = nextConfig
