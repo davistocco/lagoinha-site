7f4afa0b3738e3c09904d52b68f05f12d0a8201f
Davi Stocco <davistocco@outlook.com>
2023-11-29 11:16:50 -0300

    adding new pics to carousel

--- domains/photos.js
+++ domains/photos.js
@@ -1,6 +1,26 @@
 const getPhotos = () => new Promise((resolve, reject) => {
     const urls = [
         '/images/pray-carousel/IMG_01.jpg',
+        '/images/pray-carousel/IMG_02.jpg',
+        '/images/pray-carousel/IMG_03.jpg',
+        '/images/pray-carousel/IMG_04.jpg',
+        '/images/pray-carousel/IMG_05.jpg',
+        '/images/pray-carousel/IMG_06.jpg',
+        '/images/pray-carousel/IMG_07.jpg',
+        '/images/pray-carousel/IMG_08.jpg',
+        '/images/pray-carousel/IMG_09.jpg',
+        '/images/pray-carousel/IMG_10.jpg',
+        '/images/pray-carousel/IMG_11.jpg',
+        '/images/pray-carousel/IMG_12.jpg',
+        '/images/pray-carousel/IMG_13.jpg',
+        '/images/pray-carousel/IMG_14.jpg',
+        '/images/pray-carousel/IMG_15.jpg',
+        '/images/pray-carousel/IMG_16.jpg',
+        '/images/pray-carousel/IMG_17.jpg',
+        '/images/pray-carousel/IMG_18.jpg',
+        '/images/pray-carousel/IMG_19.jpg',
+        '/images/pray-carousel/IMG_20.jpg',
+        '/images/pray-carousel/IMG_21.jpg',
     ];
     resolve(urls.map(url => ({ url })));
 });
